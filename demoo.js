ec4a63eb6d9dc69dd3a5655ef6e22eb715b91afc
shubhatiw <shubhavr12@gmail.com>
2023-08-11 12:13:00 +0530

    adding second file

--- demoo.js
+++ demoo.js
@@ -1,1 +1,34 @@
-console.log('welcome')
+//EXAMINE THE DOCUMENT OBJECT //
+console.dir(document);
+console.log(document.domain);
+console.log(document. URL);
+console.log(document.title);
+//document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all [10]);
+// document.all [10].textContent = 'Hello';
+console.log(document.forms [0]);
+console.log(document.links);
+console.log(document.images);
+
+//get element by id
+console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>
+header.style.borderBottom = 'solid 3px #000';
+document.addEventListener("DOMContentLoaded", function() {
+    const addItemsHeading = document.querySelector(".title");
+
+    // Apply styles to the "Add Items" heading
+    addItemsHeading.style.fontWeight = "bold";
+    addItemsHeading.style.color = "green";
+});
+
